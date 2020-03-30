cd3a09aabe1172fbc708b5bf251fe6b577d59aa3
Joe Yates <joe.g.yates@gmail.com>
2020-03-30 12:29:09 +0200

    Update dependency

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule FermoHelpers.MixProject do
   use Mix.Project
 
-  @version "0.7.1"
+  @version "0.7.2"
   @git_origin "https://github.com/leanpanda-com/fermo_helpers"
 
   def project do
@@ -35,7 +35,7 @@ defmodule FermoHelpers.MixProject do
       {:deep_merge, "~> 1.0"},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:jason, "~> 1.1"},
-      {:morphix, "~> 0.0.7"},
+      {:morphix, ">= 0.8.0"},
       {:yaml_elixir, "~> 1.3.0"}
     ]
   end
